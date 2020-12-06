e2107275d915973161ac6145589316a872f3cf02
Javier Matíes <javier@geekia.es>
2020-12-06 20:16:32 +0100

    Titulo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Si Puedo - Accesibilidad, Software Libre y algo más',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Una utopia, desarrollo web bajo open source y temas personales.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
