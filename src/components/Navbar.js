8302739e085385a592e0b40456257304f4ab033c
Javier Matíes <javier@geekia.es>
2020-12-09 09:52:48 +0100

    css y menus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
+//import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -62,20 +62,11 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
-              <a className="navbar-item" href="https://www.geekia.es">
-                Empresa
-              </a>
+              
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
+              <a className="navbar-item geekia" href="https://www.geekia.es" role = "button">
+                geekia
               </a>
             </div>
           </div>
